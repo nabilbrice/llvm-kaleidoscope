e9e74a0051c3d3b2859eb1b810a861ac2a965392
Nabil Brice <nabilbrice@yahoo.co.uk>
2024-03-16 12:28:57 +0000

    modified to parse expressions of multiple binops

--- src/parser.rs
+++ src/parser.rs
@@ -46,13 +46,13 @@ struct FunctionExpr<'a> {
 }
 
 // A primitive AST node is without any sub-nodes
-fn make_primitive<'a>(tok: &Token<'a>) -> ExprAST<'a> {
+fn make_primitive<'a>(tok: Option<&Token<'a>>) -> Option<ExprAST<'a>> {
     match tok {
-        Token::Identifier(str) => ExprAST::Variable(VariableExpr { name: str }),
-        Token::Number(value) => ExprAST::Number(NumberExpr {
+        Some(Token::Identifier(str)) => Some(ExprAST::Variable(VariableExpr { name: str })),
+        Some(Token::Number(value)) => Some(ExprAST::Number(NumberExpr {
             value: value.clone(),
-        }),
-        _ => panic!("Expected primitive!"),
+        })),
+        _ => None,
     }
 }
 
@@ -60,7 +60,7 @@ fn make_primitive<'a>(tok: &Token<'a>) -> ExprAST<'a> {
 fn get_opcode<'a>(tok: &Token<'a>) -> Option<(char, i32)> {
     match tok {
         Token::Identifier("+") => Some(('+', 50)),
-        Token::Identifier("-") => Some(('-', 60)),
+        Token::Identifier("-") => Some(('-', 40)),
         Token::Identifier("*") => Some(('*', 80)),
         Token::Identifier("<") => Some(('<', 100)),
         _ => None,
@@ -74,25 +74,26 @@ fn get_opcode<'a>(tok: &Token<'a>) -> Option<(char, i32)> {
 // the prec is the precendence value
 // this currently only makes an expression with all rhs expanded
 // a parser needs to construct the total AST
-fn make_expr<'a>(tok_iter: &'a mut Peekable<TokenIter>, prec: i32) -> ExprAST<'a> {
-    let lhs = make_primitive(&tok_iter.next().unwrap());
+fn make_expr<'a>(tok_iter: &mut Peekable<TokenIter<'a>>, prec: i32) -> ExprAST<'a> {
+    let mut lhs = make_primitive(tok_iter.next().as_ref());
 
-    match get_opcode(tok_iter.peek().expect("Reached end!")) {
-        Some(opcode) => {
-            tok_iter.next();
-            let rhs: ExprAST<'a>;
-            if opcode.1 <= prec {
-                rhs = make_expr(tok_iter, prec);
-            } else {
-                rhs = make_primitive(&tok_iter.next().unwrap());
-            }
-            ExprAST::BinaryOp(Box::new(BinaryOpExpr {
-                op: opcode.0,
-                args: [lhs, rhs],
-            }))
-        }
-        None => lhs,
+    loop {
+        let (op, new_prec) = match get_opcode(tok_iter.peek().unwrap()) {
+            Some((opcode, new_prec)) => (opcode, new_prec),
+            // a negative value to ensure the break when reaching EOF
+            _ => ('#', -1000),
+        };
+        if new_prec < prec {
+            break;
+        };
+        tok_iter.next();
+        let rhs = make_expr(tok_iter, new_prec);
+        lhs = Some(ExprAST::BinaryOp(Box::new(BinaryOpExpr {
+            op,
+            args: [lhs.unwrap(), rhs],
+        })));
     }
+    lhs.unwrap()
 }
 
 #[cfg(test)]
@@ -105,11 +106,11 @@ mod tests {
         let mut tok_iter = TokenIter::new(&input).peekable();
         let mut tmp_iter = TokenIter::new(&input).peekable();
 
-        let lhs = make_primitive(tok_iter.peek().unwrap());
+        let lhs = make_primitive(tok_iter.peek()).unwrap();
         assert_eq!(ExprAST::Variable(VariableExpr { name: "x" }), lhs);
         tok_iter.next();
         tok_iter.next();
-        let rhs = make_primitive(tok_iter.peek().unwrap());
+        let rhs = make_primitive(tok_iter.peek()).unwrap();
         assert_eq!(ExprAST::Number(NumberExpr { value: 2.0 }), rhs);
 
         let result = ExprAST::BinaryOp(Box::new(BinaryOpExpr {
@@ -126,21 +127,25 @@ mod tests {
         let mut tok_iter = TokenIter::new(&input).peekable();
         let mut tmp_iter = TokenIter::new(&input).peekable();
 
-        let prim_x = make_primitive(tmp_iter.peek().unwrap());
+        let prim_x = make_primitive(tmp_iter.peek()).unwrap();
         assert_eq!(ExprAST::Variable(VariableExpr { name: "x" }), prim_x);
         tmp_iter.next();
         tmp_iter.next();
-        let prim_y = make_primitive(tmp_iter.peek().unwrap());
+        let prim_y = make_primitive(tmp_iter.peek()).unwrap();
         assert_eq!(ExprAST::Variable(VariableExpr { name: "y" }), prim_y);
 
         tmp_iter.next();
         tmp_iter.next();
-        let prim_r = make_primitive(tmp_iter.peek().unwrap());
+        let prim_r = make_primitive(tmp_iter.peek()).unwrap();
         assert_eq!(ExprAST::Number(NumberExpr { value: 2.0 }), prim_r);
 
-        let result = ExprAST::BinaryOp(Box::new(BinaryOpExpr {
+        let lhs = ExprAST::BinaryOp(Box::new(BinaryOpExpr {
             op: '+',
             args: [prim_x, prim_y],
+        }));
+        let result = ExprAST::BinaryOp(Box::new(BinaryOpExpr {
+            op: '-',
+            args: [lhs, prim_r],
         }));
         let binop = make_expr(&mut tok_iter, 0);
         assert_eq!(result, binop);
